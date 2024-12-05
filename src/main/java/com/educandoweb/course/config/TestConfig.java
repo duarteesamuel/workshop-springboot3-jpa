4c0cf6f8aca75f4c5a489f89a77a8c9d463e035c
Samuel Duarte <sduartegit@gmail.com>
2024-12-05 12:01:35 -0300

    Implementação do OrderStatus e Tratamento de erros

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.config;
 
 import java.time.Instant;
-
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -28,9 +28,9 @@ public class TestConfig implements CommandLineRunner{
 		User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order order1 = new Order(null, Instant.parse("2024-12-05T11:04:07Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2024-12-05T11:04:10Z"), user2);
-		Order order3 = new Order(null, Instant.parse("2024-12-05T11:04:22Z"), user1);
+		Order order1 = new Order(null, Instant.parse("2024-12-05T11:04:07Z"),OrderStatus.PAID, user1);
+		Order order2 = new Order(null, Instant.parse("2024-12-05T11:04:10Z"),OrderStatus.WAITING_PAYMENT, user2);
+		Order order3 = new Order(null, Instant.parse("2024-12-05T11:04:22Z"),OrderStatus.WAITING_PAYMENT, user1);
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));		
